818fae7698e3d987dc9c2cf8abd3c9041f173823
moonyechan <131453347+moonyechan@users.noreply.github.com>
2023-04-21 21:09:41 +0900

    Update pivo.cpp

--- pivo.cpp
+++ pivo.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h> 
-int main(void) {
+int main(void) 
+{
 	int a = 0, b = 1, c, n;
 
 	printf("몇번째 항까지 구할까요?");
 
 	scanf_s("%d", &n);
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
+	{
 		printf("%d ", a);
 		c = a + b;
 		a = b;
